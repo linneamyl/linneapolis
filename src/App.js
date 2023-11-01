962577d57921ea929e18ce1df97a01a49a99a411
Linnea Myllynen <linnea.myllynen@orangit.fi>
2023-11-01 21:08:04 +0200

    Show confetti when CTAbutton clicked

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from "./logo.svg";
 import "./App.css";
 import CtaButton from "./components/ctaButton";
+import Confetti from "react-confetti";
+import { useState, useRef, useEffect } from "react";
 
 const ctaContents = {
   headind: "Lorem ipsum dolor sit",
@@ -9,11 +11,36 @@ const ctaContents = {
 };
 
 function App() {
+  const [showConfetti, setShowConfetti] = useState(false);
+  const [height, setHeight] = useState(null);
+  const [width, setWidth] = useState(null);
+  const confetiRef = useRef(null);
+
+  useEffect(() => {
+    setHeight(confetiRef.current.clientHeight);
+    setWidth(confetiRef.current.clientWidth);
+  }, []);
+
   const onClick = () => {
-    console.log("jeeeee");
+    setShowConfetti(true);
   };
+
+  if (showConfetti) {
+    setTimeout(() => {
+      setShowConfetti(false);
+    }, "10000");
+  }
+
+  console.log(showConfetti);
   return (
-    <div className="App">
+    <div className="App" ref={confetiRef}>
+      {showConfetti && (
+        <Confetti
+          width={width}
+          height={height}
+          onConfettiComplete={() => setShowConfetti(false)}
+        />
+      )}
       <header className="App-header">
         <CtaButton
           image={logo}
