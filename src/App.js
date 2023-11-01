bc3f8fc2f68069dd4872a6c4f0036d0dac598f06
Linnea Myllynen <linnea.myllynen@orangit.fi>
2023-11-01 21:45:12 +0200

    small tweaks

--- src/App.js
+++ src/App.js
@@ -3,11 +3,33 @@ import "./App.css";
 import CtaContainer from "./components/ctaContainer";
 import Confetti from "react-confetti";
 import { useState, useRef, useEffect } from "react";
+import Layout from "./components/layout";
+import Body from "./components/body";
 
 const ctaContents = {
   headind: "Lorem ipsum dolor sit",
   lead: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec odio.",
   buttonLabel: "Call to action",
+  bodyText:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod \
+  tempor incididunt ut labore et dolore magna aliqua. Scelerisque varius \
+  morbi enim nunc faucibus a pellentesque. Pellentesque diam volutpat \
+  commodo sed egestas egestas fringilla. Fermentum posuere urna nec \
+  tincidunt praesent semper feugiat. Metus aliquam eleifend mi in nulla \
+  posuere sollicitudin aliquam ultrices. Orci ac auctor augue mauris \
+  augue. Vulputate dignissim suspendisse in est. Habitasse platea dictumst \
+  vestibulum rhoncus. Aliquet eget sit amet tellus cras adipiscing. Eu \
+  turpis egestas pretium aenean pharetra magna ac placerat. Tellus orci ac \
+  auctor augue mauris augue neque gravida in. Vitae elementum curabitur \
+  vitae nunc sed velit dignissim sodales. Dictum non consectetur a erat \
+  nam at lectus. Eu ultrices vitae auctor eu augue ut lectus arcu. Ornare \
+  massa eget egestas purus viverra accumsan in nisl. Urna et pharetra \
+  pharetra massa massa. Gravida in fermentum et sollicitudin ac orci \
+  phasellus. Mattis enim ut tellus elementum sagittis. Elit pellentesque \
+  habitant morbi tristique. In tellus integer feugiat scelerisque varius \
+  morbi enim nunc. Nibh venenatis cras sed felis eget velit aliquet. \
+  Euismod lacinia at quis risus sed vulputate odio. Urna molestie at \
+  elementum eu. Ante in nibh mauris cursus.",
 };
 
 function App() {
@@ -32,23 +54,26 @@ function App() {
   }
 
   return (
-    <div className="App" ref={confetiRef}>
-      {showConfetti && (
-        <Confetti
-          width={width}
-          height={height}
-          onConfettiComplete={() => setShowConfetti(false)}
-        />
-      )}
-      <header className="App-header">
-        <CtaContainer
-          image={logo}
-          heading={ctaContents.headind}
-          lead={ctaContents.lead}
-          buttonLabel={ctaContents.buttonLabel}
-          onClick={onClick}
-        />
-      </header>
+    <div ref={confetiRef}>
+      <Layout>
+        <div className="App">
+          {showConfetti && (
+            <Confetti
+              width={width}
+              height={height}
+              onConfettiComplete={() => setShowConfetti(false)}
+            />
+          )}
+          <Body text={ctaContents.bodyText} />
+          <CtaContainer
+            image={logo}
+            heading={ctaContents.headind}
+            lead={ctaContents.lead}
+            buttonLabel={ctaContents.buttonLabel}
+            onClick={onClick}
+          />
+        </div>
+      </Layout>
     </div>
   );
 }
